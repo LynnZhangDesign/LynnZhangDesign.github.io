cf7bebbccfd27c08a6dc9caa020700a576b893b2
LynnZhangDesign <zhenglin.zhang.lynn@gmail.com>
2023-03-11 04:41:03 +0000

    update

---
+++ static/js/portfolio.js
@@ -0,0 +1,356 @@
+let hive_cover = document.getElementById("hive_cover")
+$(function () {
+    $(hive_cover).on({
+        mouseover: function () {
+            setTimeout(() => {
+                if ($(hive_cover).is(':hover')) {
+                    let l1 = document.getElementById("hive_highlight")
+                    $(l1).animate({
+                        width: "175px"
+                    }, 500);
+                    hive_cover.style.transform = "scale(1.2)";
+                }
+            }, 500);
+        },
+    });
+    $(hive_cover).on({
+        mouseout: function () {
+            let l1 = document.getElementById("hive_highlight")
+            $(l1).animate({
+                width: "0px"
+            }, 500);
+            hive_cover.style.transform = "scale(1)";
+        },
+    });
+});
+let hive_font = document.getElementById("hive_font")
+$(function () {
+    $(hive_font).on({
+        mouseover: function () {
+            setTimeout(() => {
+                if ($(hive_font).is(':hover')) {
+                    let l1 = document.getElementById("hive_highlight")
+                    $(l1).animate({
+                        width: "175px"
+                    }, 500);
+                    hive_cover.style.transform = "scale(1.2)";
+                }
+            }, 500);
+        },
+    });
+    $(hive_font).on({
+        mouseout: function () {
+            let l1 = document.getElementById("hive_highlight")
+            $(l1).animate({
+                width: "0px"
+            }, 500);
+            hive_cover.style.transform = "scale(1)";
+        },
+    });
+});
+
+let beke_cover = document.getElementById("beke_cover")
+$(function () {
+    $(beke_cover).on({
+        mouseover: function () {
+            setTimeout(() => {
+                if ($(beke_cover).is(':hover')) {
+                    let l1 = document.getElementById("beke_highlight")
+                    $(l1).animate({
+                        width: "186.0px"
+                    }, 500);
+                    beke_cover.style.transform = "scale(1.13)";
+                }
+            }, 500);
+        },
+    });
+    $(beke_cover).on({
+        mouseout: function () {
+            let l1 = document.getElementById("beke_highlight")
+            $(l1).animate({
+                width: "0px"
+            }, 500);
+            beke_cover.style.transform = "scale(1)";
+        },
+    });
+});
+let beke_font = document.getElementById("beke_font")
+$(function () {
+    $(beke_font).on({
+        mouseover: function () {
+            setTimeout(() => {
+                if ($(beke_font).is(':hover')) {
+                    let l1 = document.getElementById("beke_highlight")
+                    $(l1).animate({
+                        width: "186.0px"
+                    }, 500);
+                    beke_cover.style.transform = "scale(1.13)";
+                }
+            }, 500);
+        },
+    });
+    $(beke_font).on({
+        mouseout: function () {
+            let l1 = document.getElementById("beke_highlight")
+            $(l1).animate({
+                width: "0px"
+            }, 500);
+            beke_cover.style.transform = "scale(1)";
+        },
+    });
+});
+
+let fireworks_cover = document.getElementById("fireworks_cover")
+$(function () {
+    $(fireworks_cover).on({
+        mouseover: function () {
+            setTimeout(() => {
+                if ($(fireworks_cover).is(':hover')) {
+                    let l1 = document.getElementById("fireworks_highlight")
+                    $(l1).animate({
+                        width: "529.6px"
+                    }, 500);
+                    fireworks_cover.style.transform = "scale(1.2)";
+                }
+            }, 500);
+        },
+    });
+    $(fireworks_cover).on({
+        mouseout: function () {
+            let l1 = document.getElementById("fireworks_highlight")
+            $(l1).animate({
+                width: "0px"
+            }, 500);
+            fireworks_cover.style.transform = "scale(1)";
+        },
+    });
+});
+let fireworks_font = document.getElementById("fireworks_font")
+$(function () {
+    $(fireworks_font).on({
+        mouseover: function () {
+            setTimeout(() => {
+                if ($(fireworks_font).is(':hover')) {
+                    let l1 = document.getElementById("fireworks_highlight")
+                    $(l1).animate({
+                        width: "529.6px"
+                    }, 500);
+                    fireworks_cover.style.transform = "scale(1.2)";
+                }
+            }, 500);
+        },
+    });
+    $(fireworks_font).on({
+        mouseout: function () {
+            let l1 = document.getElementById("fireworks_highlight")
+            $(l1).animate({
+                width: "0px"
+            }, 500);
+            fireworks_cover.style.transform = "scale(1)";
+        },
+    });
+});
+
+let furry_cover = document.getElementById("furry_cover")
+$(function () {
+    $(furry_cover).on({
+        mouseover: function () {
+            setTimeout(() => {
+                if ($(furry_cover).is(':hover')) {
+                    let l1 = document.getElementById("furry_highlight")
+                    $(l1).animate({
+                        width: "469.7px"
+                    }, 500);
+                    furry_cover.style.transform = "scale(1.2)";
+                }
+            }, 500);
+        },
+    });
+    $(furry_cover).on({
+        mouseout: function () {
+            let l1 = document.getElementById("furry_highlight")
+            $(l1).animate({
+                width: "0px"
+            }, 500);
+            furry_cover.style.transform = "scale(1)";
+        },
+    });
+});
+let furry_font = document.getElementById("furry_font")
+$(function () {
+    $(furry_font).on({
+        mouseover: function () {
+            setTimeout(() => {
+                if ($(furry_font).is(':hover')) {
+                    let l1 = document.getElementById("furry_highlight")
+                    $(l1).animate({
+                        width: "469.7px"
+                    }, 500);
+                    furry_cover.style.transform = "scale(1.2)";
+                }
+            }, 500);
+        },
+    });
+    $(furry_font).on({
+        mouseout: function () {
+            let l1 = document.getElementById("furry_highlight")
+            $(l1).animate({
+                width: "0px"
+            }, 500);
+            furry_cover.style.transform = "scale(1)";
+        },
+    });
+});
+
+let iwen_cover = document.getElementById("iwen_cover")
+$(function () {
+    $(iwen_cover).on({
+        mouseover: function () {
+            setTimeout(() => {
+                if ($(iwen_cover).is(':hover')) {
+                    let l1 = document.getElementById("iwen_highlight")
+                    $(l1).animate({
+                        width: "186.1px"
+                    }, 500);
+                    iwen_cover.style.transform = "scale(1.2)";
+                }
+            }, 500);
+        },
+    });
+    $(iwen_cover).on({
+        mouseout: function () {
+            let l1 = document.getElementById("iwen_highlight")
+            $(l1).animate({
+                width: "0px"
+            }, 500);
+            iwen_cover.style.transform = "scale(1)";
+        },
+    });
+});
+let iwen_font = document.getElementById("iwen_font")
+$(function () {
+    $(iwen_font).on({
+        mouseover: function () {
+            setTimeout(() => {
+                if ($(iwen_font).is(':hover')) {
+                    let l1 = document.getElementById("iwen_highlight")
+                    $(l1).animate({
+                        width: "186.1px"
+                    }, 500);
+                    iwen_cover.style.transform = "scale(1.2)";
+                }
+            }, 500);
+        },
+    });
+    $(iwen_font).on({
+        mouseout: function () {
+            let l1 = document.getElementById("iwen_highlight")
+            $(l1).animate({
+                width: "0px"
+            }, 500);
+            iwen_cover.style.transform = "scale(1)";
+        },
+    });
+});
+
+let ia_cover = document.getElementById("ia_cover")
+$(function () {
+    $(ia_cover).on({
+        mouseover: function () {
+            setTimeout(() => {
+                if ($(ia_cover).is(':hover')) {
+                    let l1 = document.getElementById("ia_highlight")
+                    $(l1).animate({
+                        width: "540.0px"
+                    }, 500);
+                    ia_cover.style.transform = "scale(1.13)";
+                }
+            }, 500);
+        },
+    });
+    $(ia_cover).on({
+        mouseout: function () {
+            let l1 = document.getElementById("ia_highlight")
+            $(l1).animate({
+                width: "0px"
+            }, 500);
+            ia_cover.style.transform = "scale(1)";
+        },
+    });
+});
+let ia_font = document.getElementById("ia_font")
+$(function () {
+    $(ia_font).on({
+        mouseover: function () {
+            setTimeout(() => {
+                if ($(ia_font).is(':hover')) {
+                    let l1 = document.getElementById("ia_highlight")
+                    $(l1).animate({
+                        width: "540.0px"
+                    }, 500);
+                    ia_cover.style.transform = "scale(1.13)";
+                }
+            }, 500);
+        },
+    });
+    $(ia_font).on({
+        mouseout: function () {
+            let l1 = document.getElementById("ia_highlight")
+            $(l1).animate({
+                width: "0px"
+            }, 500);
+            ia_cover.style.transform = "scale(1)";
+        },
+    });
+});
+
+let next0 = document.getElementById("next0")
+$(function () {
+    $(next0).on({
+        mouseover: function () {
+            let l1 = document.getElementById("next1")
+            let l2 = document.getElementById("next2")
+            $(l1).animate({
+                opacity: 1
+            }, 200);
+            $(l2).animate({
+                opacity: 0
+            }, 200);
+        },
+        mouseout: function () {
+            let l1 = document.getElementById("next1")
+            let l2 = document.getElementById("next2")
+            $(l1).animate({
+                opacity: 0
+            }, 200);
+            $(l2).animate({
+                opacity: 1
+            }, 200);
+        }
+    });
+});
+
+var flag = 0
+
+function next_project() {
+    let l1 = document.getElementById("work1")
+    let l2 = document.getElementById("work2")
+    if (flag === 0) {
+        $(l1).animate({
+            opacity: 0
+        }, 200);
+        $(l2).animate({
+            opacity: 1
+        }, 200);
+        flag = 1;
+    } else {
+        $(l1).animate({
+            opacity: 1
+        }, 200);
+        $(l2).animate({
+            opacity: 0
+        }, 200);
+        flag = 0;
+    }
+    return
+}
